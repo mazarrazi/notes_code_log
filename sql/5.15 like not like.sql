d29386c1b024437d5a5f8c15650cc0f7060ff03c
Siva ganesh <119166009+mazarrazi@users.noreply.github.com>
2024-02-16 21:17:49 +0530

    Create 5.15 like not like.sql

---
+++ sql/5.15 like not like.sql
@@ -0,0 +1,28 @@
+# 1. Working with the “employees” table, use the LIKE operator to select the data about all individuals, whose first name starts with “Mark”; specify that the name can be succeeded by any sequence of characters.
+
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    first_name LIKE ('Mark%');
+
+
+# 2. Retrieve a list with all employees who have been hired in the year 2000.
+
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    hire_date LIKE ('%2000%');
+
+
+# 3. Retrieve a list with all employees whose employee number is written with 5 characters, and starts with “1000”.
+
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    emp_no LIKE ('1000_');
