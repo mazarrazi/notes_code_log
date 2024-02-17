554407d2042c366c7454224a6a0cf99ad4676618
Siva ganesh <119166009+mazarrazi@users.noreply.github.com>
2024-02-17 08:45:32 +0530

    Update 5.17 wildcard characters.sql

--- sql/5.17 wildcard characters.sql
+++ sql/5.17 wildcard characters.sql
@@ -1,24 +1,24 @@
 # 1. Extract all individuals from the ‘employees’ table whose first name contains “Jack”.
 
-```MySQL
+MySQL
 SELECT 
     *
 FROM
     employees
 WHERE
     first_name LIKE ('%jack%');
-```
+
 
 
 # 2. Once you have done that, extract another list containing the names of employees that do not contain “Jack”.
 
-```MySQL
+MySQL
 SELECT 
     *
 FROM
     employees
 WHERE
     first_name NOT LIKE ('%jack%');
-```
+
 
 
