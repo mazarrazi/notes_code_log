593ecb318ec799238c880ed7fb8959427c3f5706
Siva ganesh <119166009+mazarrazi@users.noreply.github.com>
2024-02-19 11:53:48 +0530

    Update 5.23 other comparison operators.sql

--- sql/5.23 other comparison operators.sql
+++ sql/5.23 other comparison operators.sql
@@ -1,10 +1,5 @@
-# 1. Retrieve a list with data about all 
-# female employees who were hired in the 
-# year 2000 or after.
-# (If you solve the task correctly, SQL
-# should return 7 rows)
-
-
+# 1. Retrieve a list with data about all female employees who were hired in the year 2000 or after. (If you solve the task correctly, SQL should return 7 rows)
+    
 SELECT 
     hire_date, gender
 FROM
@@ -13,9 +8,7 @@ WHERE
     (gender = 'F') AND (hire_date >= '2000-01-01')  ;
 
 
-# 2. Extract a list with all employees’ 
-# salaries higher than $150,000 per annum.
-
+# 2. Extract a list with all employees’ salaries higher than $150,000 per annum.
 
 SELECT 
     *
