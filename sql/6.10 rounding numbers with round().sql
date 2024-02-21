0b1e86bb3c174098cedf008c0da45af81c8446ef
Siva ganesh <119166009+mazarrazi@users.noreply.github.com>
2024-02-21 08:06:46 +0530

    Update 6.10 rounding numbers with round().sql

--- sql/6.10 rounding numbers with round().sql
+++ sql/6.10 rounding numbers with round().sql
@@ -1,1 +1,8 @@
+# Round the average amount of money spent on salaries for all contracts that started after the 1st of January 1997 to a precision of cents.
 
+SELECT
+	ROUND(AVG(salary), 2)
+FROM
+	salaries
+WHERE
+	from_date > '1997-01-01' ;
