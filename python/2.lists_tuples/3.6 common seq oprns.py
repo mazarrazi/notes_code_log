1d60d1d59f57e6a6ea595c0d07c4b799cc773272
Siva ganesh <119166009+mazarrazi@users.noreply.github.com>
2024-02-21 18:42:27 +0530

    Create 3.6 common seq oprns.py

---
+++ python/2.lists_tuples/3.6 common seq oprns.py
@@ -0,0 +1,13 @@
+even = ["2", "4", "6", "8"]  
+odd = ["1", "3", "5", "7", "9"]  
+  
+print(max(even))  
+print(min(even))  
+print(max(odd))  
+print(min(odd))  
+  
+print(len(even))  
+print(len(odd))  
+  
+print("mississippi".count("i"))  
+print("mississippi".count("ssi"))
