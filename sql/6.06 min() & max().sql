aa208fbe54c1b16d8f88ca7bff9a8cb307821f68
Siva ganesh <119166009+mazarrazi@users.noreply.github.com>
2024-02-21 08:03:45 +0530

    Create 6.06 min() & max().sql

---
+++ sql/6.06 min() & max().sql
@@ -0,0 +1,14 @@
+# 1. Which is the lowest employee number in the database?
+
+SELECT
+	MIN(emp_no)
+FROM
+	dept_emp ;
+
+
+# 2. Which is the highest employee number in the database?
+
+SELECT
+	MAX(emp_no)
+FROM
+	dept_emp ;
