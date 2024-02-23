e768de126afb8765ccbc8bbb4f895e1527a62301
Siva ganesh <119166009+mazarrazi@users.noreply.github.com>
2024-02-23 23:24:43 +0530

    Create _3_43_2_ jukebox_ import_trial.py

---
+++ python/2.lists_tuples/_3_43_2_ jukebox_ import_trial.py
@@ -0,0 +1,8 @@
+# choose an album from a list of albums  
+# from the album chosen, show list of songs  
+# after song is chosen, print "song is playing"  
+# 0 is invalid, any other no. than the provided options are invalid.  
+# invalid option leads to exit  
+  
+from _3_43_1_data_import_for_jukebox import albums  
+print(albums)
