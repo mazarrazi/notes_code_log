1269a2e8d81133b84620e3cc2976996541e64691
Siva ganesh <119166009+mazarrazi@users.noreply.github.com>
2024-02-23 23:15:52 +0530

    Create 3.37 more unpacking.py

---
+++ python/2.lists_tuples/3.37 more unpacking.py
@@ -0,0 +1,23 @@
+welcome = "Welcome to my Nightmare", "Alice Cooper", 1975  
+bad = "Bad Company", "Bad Company", 1974  
+budgie = "Night flight", "Budgie", 1981  
+imelda = "More Mayhem", "Emilda May", 2011  
+metallica = "Ride the Lightning", "Metallica", 1984  
+  
+# print(metallica)  
+# print(metallica[0])  # even for tuple [] is used for index operations  
+# print(metallica[1])  
+# print(metallica [2])  
+  
+title, artist, year = metallica  
+print(title)  
+print(artist)  
+print(year)  
+  
+print()  
+  
+table = ("coffee table", 200, 100, 75, 34.50)  
+print(table[1] * table[2])  
+  
+name, length, width, height, price = table  
+print(length * width)
