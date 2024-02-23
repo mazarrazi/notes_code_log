5651337ff529799509d913e1ff38c2139a03b3ed
Siva ganesh <119166009+mazarrazi@users.noreply.github.com>
2024-02-23 23:17:50 +0530

    Create _4_2_Defining_a_function.py

---
+++ python/3.functions/_4_2_Defining_a_function.py
@@ -0,0 +1,10 @@
+  
+def multiplyy():  
+	result = 10.2 * 8  
+	return result  
+  
+  
+ans = multiplyy() # the return value from the function is bound to a variable, we will see why its needed  
+print(ans)  
+  
+# NOTE: leave two blank lines before and after function definition
